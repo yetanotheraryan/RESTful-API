24b003e4ce13cbb5abf664c6609f1d6099a7d0cd
aryan-tiwari <aryantiwari91000@gmail.com>
2020-09-12 00:00:53 +0530

    Patch functionality added, final commit

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -34,8 +34,19 @@ router.post('/', async(req, res)=>{
     }
 })
 // updating one
-router.patch('/:id', (req, res)=>{
-
+router.patch('/:id', getSubscriber ,async(req, res)=>{
+    if(req.body.name != null){
+        res.subscriber.name = req.body.name
+    }
+    if(req.body.subscribedToChannel != null){
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try{
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber)
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
 });
 // deleting one
 router.delete('/:id', getSubscriber, async(req, res)=>{
