d37087c590cf1f26e892eadaef6151f72866694a
aryan-tiwari <aryantiwari91000@gmail.com>
2020-09-11 22:59:46 +0530

    post route working

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,20 +1,34 @@
 const express = require("express")
 const router = express.Router()
-
+const Subscriber = require('../models/subscriber')
 // getting all
-router.get('/', (req, res)=>{
-    res.send("Hello world");
+router.get('/', async(req, res)=>{
+    try{
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
 })
 // getting one
 
 // :id, means inside the callback the id using req.params.id
 router.get('/:id', (req, res)=>{
-
+    res.send(req.params.id);
 })
 
 // create route 
-router.post('/', (req, res)=>{
-
+router.post('/', async(req, res)=>{
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try{
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
 })
 // updating one
 router.patch('/:id', (req, res)=>{
